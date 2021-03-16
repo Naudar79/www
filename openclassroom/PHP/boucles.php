038f4251e3fecd955a59dd543cf6dd15f2deaebf
Naudar79 <naudar79@gmail.com>
2021-03-16 16:18:35 +0100

    Update boucles.php

---
+++ openclassroom/PHP/boucles.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ $repeat = 0;
+ while ($repeat < 10) {
+    echo '<p>On ne doit pas copier sur son voisin '. $repeat .' fois</p>';
+    $repeat++ ; // équifaut à $repeat = $repeat +1
+ }
+*/
+ for ($i=0 ; $i < 10 ; $i++) { 
+    echo '<p>On ne doit toujours pas copier sur son voisin ' . $i . ' fois</p>';
+ }
+?>
