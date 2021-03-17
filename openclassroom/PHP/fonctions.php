a9c01b1f76adea1ee264fb1a2d2eab6239fb067e
Naudar79 <naudar79@gmail.com>
2021-03-17 17:06:43 +0100

    Update fonctions.php

---
+++ openclassroom/PHP/fonctions.php
@@ -0,0 +1,25 @@
+<?php // Cette fonction calcule le nombre de caractères dans une phrase
+ $phrase = 'Bonjour je suis une phrase.';
+ $nbCar = strlen($phrase);
+ echo '<p> Il y a ' . $nbCar . ' caracteres dans cette phrase. </p>';
+?>
+
+<?php // Cette fonction mélange au hasard les caractères d'une phrase
+ $phrase = 'Bonjour je suis une phrase.';
+ $phraseMelangee = str_shuffle($phrase);
+ echo '<p>La phrase: '.$phrase. ' donne dans le désordre: ' . $phraseMelangee .'</p>';
+ echo str_shuffle($phrase); // verssion raccourcie qui utilise la fonciton et l'affiche
+ echo str_shuffle('Ma phrase'); // on peut directement écrire la phrase
+?>
+
+<?php //Fonction d'affichage de la date
+ echo date('d/m/Y');
+?>
+
+<?php
+
+ function bonjour($nom){ //Declaration de la fonction
+    echo 'Bonjour ' . $nom;
+ }
+  bonjour('Marie');
+?>
