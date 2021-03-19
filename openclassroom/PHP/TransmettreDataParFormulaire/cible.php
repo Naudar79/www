1a99e253220865cbb531771b44075164a123a612
Naudar79 <naudar79@gmail.com>
2021-03-19 16:12:21 +0100

    .

---
+++ openclassroom/PHP/TransmettreDataParFormulaire/cible.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cible.php</title>
+</head>
+<body>
+    <p>Bonjour <?php echo $_POST['prenom'];?></p>
+</body>
+</html>
---
+++ openclassroom/PHP/TransmettreDataParFormulaire/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index.php</title>
+</head>
+<body>
+  <form action="cible.php" method="POST">
+    <p><label for="Prénom"><input type="text" name="prenom"></label></p>
+    <p><input type="submit"></p>
+  </form>
+</body>
+</html>
